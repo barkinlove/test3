d66765efcdbe826ed37f8ed9f2ca6a93822b1ae6
Egor Gavrilov <ejs.defolt@gmail.com>
2024-05-12 23:49:23 +0500

    fix: remove obsolete comment

--- src/main.cxx
+++ src/main.cxx
@@ -9,10 +9,7 @@ int main(int argc, char* argv[]) {
   try {
     auto& params = cmd_line_params::parse(argc, argv);
 
-    // params dont work, figure out why
-
     module_executor::work(params.work_module.getValue(), params);
-
   } catch (std::exception& e) {
     std::cerr << e.what() << '\n';
     return EXIT_FAILURE;
